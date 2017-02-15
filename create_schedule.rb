c166cb99b6f94f8200bce669ca36e6ee74127428
lyle Sanders <lylesanders66@yahoo.com>
2017-02-15 15:07:40 -0500

    outputting student csv

--- create_schedule.rb
+++ create_schedule.rb
@@ -1,11 +1,20 @@
 require"csv"
 
-def read_csv_file
+def read_student_csv_file(student_array)
 	# old_class_file = file.open("classes.csv","r") #opens the list of available classes
 	# class_file = "class_members.csv" # moves student names into the class from classes.csv
+		#student_array =[]
 		CSV.foreach ("student.csv")do |row|
-		puts row.inspect
+		student_array << row
 	end
+	# student_array= Array.new
+	# student_array = CSV.parse("student.csv")
+	
 end
+student_array= Array.new
+read_student_csv_file(student_array)
 
-read_csv_file
+# c = student_array.count
+# puts c
+puts student_array[1]
+puts student_array[0]
