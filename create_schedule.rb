cd027840944f48ae7f00a8bb3ea26fe24daf3cfd
lyle Sanders <lylesanders66@yahoo.com>
2017-02-18 18:46:33 -0500

    added large student file

--- create_schedule.rb
+++ create_schedule.rb
@@ -3,20 +3,17 @@ require"csv"
 def read_student_csv_file(file_name)
 
 	CSV.foreach(file_name) do |row|# reads the csv line by line
-    puts row.inspect    
+	puts"PROCESSING..."
+    #puts row.inspect  
+	puts row[0]	  
 	puts row[1]
 	puts row[2]
 	puts row[3]
 	puts row[4]
-	puts row[0]
 	end
 	
 end
 student_array= Array.new
-file_name ='megan_smith.csv'
+file_name ='students_file.csv'
 read_student_csv_file(file_name)
 
-c = student_array.count
-puts c
-puts student_array[1]
-puts student_array[0]
