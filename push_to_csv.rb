5b27e8fb9a6e3e0000e7b2a9d09ea2aa4f95f6b2
Don Kingery <kingery1@suddenlink.net>
2017-02-18 14:38:21 -0500

    added push file

---
+++ push_to_csv.rb
@@ -0,0 +1,11 @@
+#here we need input from user interface
+
+#then we shove it into our array
+
+#then we send array info to csv
+
+require 'csv'
+CSV.open("student.csv", "w") do |csv|
+  csv << ["David", "Jones", "Jones@email.com", "robotics", "emt", "firefighting", "coding"]
+  csv << ["Smith", "Jenny", "Smith@email.com","emt", "firefighting", "coding", "robotics" ]
+end
