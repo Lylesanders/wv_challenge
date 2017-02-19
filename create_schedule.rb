14cf8949cf054adcb4e0130843cb81fdb90a0163
lyle Sanders <lylesanders66@yahoo.com>
2017-02-19 10:32:57 -0500

    parsing csv file

--- create_schedule.rb
+++ create_schedule.rb
@@ -10,8 +10,21 @@ def read_student_csv_file(file_name)
 	puts row[2]
 	puts row[3]
 	puts row[4]
+	scheduling(row)
 	end
 	
+end
+
+def scheduling(row)
+if row[1] =="1" 
+	puts "coding"
+	elsif row[2]== "1"
+	puts "robotics"
+	elsif row[3] =="1"
+	puts "fire-fighting"
+	else
+	puts"emt"
+	end
 end
 student_array= Array.new
 file_name ='students_file.csv'
