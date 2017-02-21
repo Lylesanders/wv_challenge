bb766c7f4c6ec00025400d099b6e5d83cb056e73
lyle Sanders <lylesanders66@yahoo.com>
2017-02-20 23:05:36 -0500

    odd problem with array   pointer

--- create_schedule.rb
+++ create_schedule.rb
@@ -1,36 +1,57 @@
 require"csv"
 
 def read_student_csv_file(file_name)
+student_list = CSV.read(file_name) # this line moves csv file into customer array
 
-	CSV.foreach(file_name) do |row|# reads the csv line by line
-	puts"PROCESSING..."
-    #puts row.inspect  
-	puts row[0]	  
-	puts row[1]
-	puts row[2]
-	puts row[3]
-	puts row[4]
-	scheduling(row)
-	end
 	
+	#return student_list
+	student_randomizer(student_list)	
+
 end
 
-def scheduling(row)
-if row[1] =="1" 
-	puts "coding"
-	CSV.open('coding.csv','w') do |csv|
-	stringer = row[0]
-	csv << stringer
-	end
-	elsif row[2]== "1"
+def student_randomizer(student_list)
+	
+	puts"student randomizer"
+	a = student_list.length
+until a == 0 do
+		a=student_list.length
+		pointer = rand(0..a)
+		#puts student_list[pointer]
+		#student = student_list[pointer]
+		#at this point pass random sutudent to scheduling
+		student_list.delete_at(pointer)
+		a = student_list.length
+	
+		puts "line 25 array size = #{a}"
+		#puts student_list.length
+	end #whilee
+end #student_randomizer
+
+def scheduling(student)
+if student[1] =="1" 
+	puts "coding"	
+	
+	elsif student[2]== "1"
 	puts "robotics"
-	elsif row[3] =="1"
+	elsif student[3] =="1"
 	puts "fire-fighting"
 	else
 	puts"emt"
 	end
 end
-student_array= Array.new
-file_name ='student.csv'
-read_student_csv_file(file_name)
 
+def load_classes(class_file)
+	classes = CSV.read(class_file)
+	#puts "  line 45 #{classes.length}"
+end #load_classes
+
+
+student_list = []
+puts "beginning"
+# file_name ='students_file.csv'
+file_name='student.csv'
+class_file = 'classes.csv'
+load_classes(class_file)
+read_student_csv_file(file_name)
+puts "at controll student list length = #{student_list.length}"
+student_randomizer(student_list)
---
+++ runner.rb
@@ -0,0 +1,12 @@
+#require "CSV"
+
+require_relative"create_schedule.rb"
+student_list = Array.new
+# puts "beginning"
+#file_name ='students_file.csv'
+file_name='student.csv'
+class_file = 'classes.csv'
+load_classes(class_file)
+read_student_csv_file(file_name)
+#puts "at controll student list length = #{student_list.length}"
+#student_randomizer(student_list)
