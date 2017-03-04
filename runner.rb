07c96c624dbee011b3c26021463ca649afd8caa0
lyle Sanders <lylesanders66@yahoo.com>
2017-03-04 16:24:40 -0500

    update 3417 before changes for fname lname

--- runner.rb
+++ runner.rb
@@ -1,6 +1,16 @@
 require_relative 'scheduler'
 
+require_relative 'scheduler'
+#(1..1000).each do |count_loop|
 current_schedule = Schedule.new
-@top_five = [["z",1],["x",2],["y",3],["w",4],["v",5]]
-current_schedule.sort_top_five(@top_five)
-current_schedule.weighted_schedule(@scheduled)
+file_name = "students_file.csv" # can be made dynamic
+class_file = "classes.csv" # can be made dynamic
+output_file = "foo.csv"
+current_schedule.master_student_list(file_name)
+current_schedule.master_class_list(class_file)
+until current_schedule.student_list.length == 0 do 
+	current_schedule.randomizer(@student_list) 
+    current_schedule.scheduler(@student,@class_list)
+
+
+end #until
--- scheduler.rb
+++ scheduler.rb
@@ -14,20 +14,22 @@ class Schedule #oh the irony
 	def initialize
 	    @student_list = Array.new # handed off to randomizer to generate the student variable 
 	    @class_list = Array.new # handed off to scheduler used to generate output in scheduled
+	    @scheduled =Array.new	    
 		@top_five = Array.new(5,["a",0]) 
 	    @student = student
-	    @scheduled =Array.new
 	    @weight = weight
-	    @accum =accum
+
 	end #initialize
 
 	def master_student_list(file_name)
+
 	@student_list = CSV.read(file_name) # this line moves csv file into customer array	
 	#values are sent to randomizer
 
 	end
 
 	def master_class_list(class_file)
+
 		@class_list = CSV.read(class_file)
 	# https://www.sitepoint.com/guide-ruby-csv-library-part/
 		a = @class_list.length
@@ -42,17 +44,17 @@ class Schedule #oh the irony
 
 	 def randomizer(student_list) # def randomizer(student_list, class_list) #
 
+
 		a= @student_list.length
 		pointer = rand(0..a-1)			
 		@student = @student_list[pointer] 
 		@student_list.delete_at(pointer)
-
-		# scheduler(@student,@class_list)
 		return @student # student is handed off to scheduler
 
 	end #randomizer
 
 def scheduler(student,class_list) # def scheduler(student,class_list,scheduled) #after testing
+
 	output = []
 	xdx = 0
 	tracking = false
@@ -61,62 +63,63 @@ def scheduler(student,class_list) # def scheduler(student,class_list,scheduled) 
 	@student.each_with_index do |lmnt,ndx|
 
 		@class_list.each_with_index do |element,indx|
-			
 			if lmnt==element[0] and element[1]>0
 				xdex = 5-ndx
-				output =[@student[0],element[0],xdex]
+				output =[@student[0],element[0],xdex.to_i]
 				element[1] -=1
 				tracking = true
-				break
-			end
-		end
+			
+				break # break class_list .each_with index do
+			end #if
+		end #class_list.each with index
 		if tracking == true
 			break
-		end
-	end		
+		end #if
+	end # student.each_with_index
 
-	#puts output.inspect
-	output
-	@scheduled.push output
+
+		@scheduled.push output
+
 	return @scheduled
 end #scheduler
 
 def weighted_schedule(scheduled)
 
-	@weight = @scheduled.map(&:last).inject(:+)
-	
-
+	b = 0
+	# @weight = @scheduled.map(&:last).inject(:+)
+	@scheduled.each_with_index do |element, indx|
+		a = element[2]
+		b =b.to_i + a.to_i
+		# puts b
+	end # each_with index
+		@weight= b
  	return @weight
  end
 
  def compair_schedules?(top_five, weight)
+
 	@weight > @top_five[4][1]
-	# puts @weight.inspect
+
  end
  
 
 
 def delete_bottom_schedule(top_five)
 
-	to_delete = @top_five[4][0]
-	puts to_delete
+	to_delete = @top_five[4][0]	
 	#need to create folder and use path command to point at
 	if File.exists?(to_delete)
 		file.delete(to_delete)
-	else
-		puts "file #{to_delete} does not exist"
 	end
 end
 
 def make_current_schedule(count_loop, scheduled)
-	puts "here I am"
 	a = count_loop.to_s
 	b = "_schedule.csv"
 	create_file_name =a+b
-	puts create_file_name
+
 	if File.exists?(create_file_name)
 		File.delete(create_file_name)
-		puts "uh oh elroy"
 	end
 	CSV.open(create_file_name, "w") do |csv|
 		csv << @scheduled
@@ -132,9 +135,10 @@ def update_top_five(count_loop,top_five,weight)
 end
 
 def sort_top_five(top_five)
+
 	@top_five = @top_five.sort_by(&:last)
 	@top_five.each_with_index do |element,ndx|
-		puts element.inspect
+
 	end # each_with_index
 end #sort top five
 
