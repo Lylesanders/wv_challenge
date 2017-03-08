788621384b4db4c1361f4db2c08c61729ba23875
lyle Sanders <lylesanders66@yahoo.com>
2017-03-08 09:37:32 -0500

    added landing page and submit

--- app.rb
+++ app.rb
@@ -1,9 +1,28 @@
 require "sinatra"
 
+#landing page
+
 get "/" do
+	erb :landing_page
+end
+# post '/landing_page' do
+	
+# 	erb :student_form
+# end
+
+get '/post' do
+	# "hello world"
 	erb :student_form2
 end
 
-post 'users_fname' do
-	"thank you"
+post '/users_fname' do
+	fname = params[:fname]
+	lname = params[:lname]
+	first_choice =[:first_choice]
+    second_choice =[:second_choice]
+   	third_choice =[:second_choice]
+    fourth_choice =[:fourth_choice]
+
+
+	erb:thank_you, :locals => {:fname => fname}
 end
---
+++ make_student.rb
@@ -0,0 +1,10 @@
+require 'csv'
+
+def make_student_file(student_data)
+    CSV.open('make_student.csv','w') do |csv|
+        csv << student_data
+    end
+end
+#**********
+student_data = ["McNeeley","Jaimie","Fire_fighting","EMT","Coding","Robotics"]
+make_student_file(student_data)
