94d5485c301a0b404589c40acfbabcbbdc78b814
lyle Sanders <lylesanders66@yahoo.com>
2017-03-08 14:51:24 -0500

    updating for erb file

--- app.rb
+++ app.rb
@@ -30,15 +30,6 @@ post '/users_fname' do
 	erb:thank_you, :locals =>{:fname => fname, :lname => lname, :first_choice => first_choice, :second_choice => second_choice, :third_choice => third_choice, :fourth_choice => fourth_choice}
 end
 
-get '/student_form' do
-	submit_form = params[:submit_form]
-	if submit_form == "YES"
-		puts "\a"
-		erb :landing_page
-		# student_data = [fname,lname,first_choice,second_choice,third_choice,fourth_choice]
-		# make_student_file(student_data)
-		# erb :landing_page
-	else 
-		redirect'/:student_form2'
-	end
+post '/thank_you' do
+	erb :landing_page
 end
